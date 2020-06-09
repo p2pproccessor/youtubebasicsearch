43dacdfecceb1b9e2ecc60b7ded64637929e8f3b
p2pproccessor <66640547+p2pproccessor@users.noreply.github.com>
2020-06-09 21:08:03 +0300

    Update search.js

--- search.js
+++ search.js
@@ -79,8 +79,7 @@ function showResults(results) {
         var aciklama = value.snippet.description;
         var kanal = value.snippet.channelTitle;
         var tarih = value.snippet.publishedAt;
-        var resim_m = value.snippet.thumbnails.medium.url;
-
+        var resim_h = value.snippet.thumbnails.high.url;
         var videoId = value.id.videoId;
         
         // burada foreach dönmeye devam ettikçe html değişkenimize yeni kayıtları da ekliyoruz
@@ -89,8 +88,7 @@ function showResults(results) {
         html += 'aciklama: ' + aciklama + '<br>';
         html += 'kanal: ' + kanal + '<br>';
         html += 'tarih: ' + tarih + '<br>';
-        
-        html += '<img src="' + resim_m + '">';
+        html += '<img src="' + resim_h + '"><br><hr>';
         
     }); 
     
